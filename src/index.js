754857dba92e34c94b7799ed88bdca091e95d5b1
Julia <chijonok.julia17@gmail.com>
2023-10-22 01:49:32 +0300

    feat: add implementantion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const pair = [];
+  let current  = 0;
+
+  bracketsConfig.forEach((item) => pair.push(item[0] + item[1]));
+  console.log(pair);
+
+  while (pair.length > current) {
+      if (str.includes(pair[current])) {
+          str = str.replace(pair[current], '');
+          console.log(str);
+          current = 0;
+      } else {
+        current++;
+      }
+  }
+
+  return str === '';
+};
